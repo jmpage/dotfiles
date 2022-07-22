359f03fd3b573e9cd47624c03226ce4467b31670
Jennifer Page <jenipage1989+github@gmail.com>
2022-07-21 21:49:34 -0400

    Move emacs.d/personal from dotfiles to yadm

---
+++ .config/prelude/personal/personal.el
@@ -0,0 +1,4 @@
+(setq confirm-kill-emacs 'y-or-n-p)
+(scroll-bar-mode -1)
+(tooltip-mode nil)
+(setq mouse-yank-at-point nil)
---
+++ .config/prelude/personal/preload/prefer-melpa-stable.el
@@ -0,0 +1,11 @@
+;; See https://github.com/bbatsov/prelude/issues/674#issuecomment-255596298
+(require 'package)
+
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(setq package-archive-priorities
+      '(("melpa-stable" .  10)
+        ("melpa" . 9)
+        ))
