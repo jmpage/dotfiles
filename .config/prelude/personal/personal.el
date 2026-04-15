0eaff09257ac8ae75afe87fc4576221584f6fed2
Jennifer Page <2141169+jmpage@users.noreply.github.com>
2026-04-14 20:05:28 -0400

    emacs: modifier key changes for macos, lsp warning suppression & golang indentation

--- .config/prelude/personal/personal.el
+++ .config/prelude/personal/personal.el
@@ -3,3 +3,15 @@
 (tooltip-mode nil)
 (menu-bar-mode -1)
 (setq mouse-yank-at-point nil)
+
+(when (eq system-type 'darwin)
+  (setq mac-control-modifier 'control)
+  (setq mac-command-modifier 'super)
+  (setq mac-option-modifier 'meta)
+)
+
+(add-to-list 'warning-suppress-log-types '(lsp-mode))
+(add-to-list 'warning-suppress-types '(lsp-mode))
+
+(add-hook 'go-mode-hook (lambda ()
+                          (setq tab-width 4)))
